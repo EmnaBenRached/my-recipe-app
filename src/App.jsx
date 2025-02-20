e8d514ab9bc028b5adc1719dc6515cfbdc331f0c
EmnaDev <b.rached.emna@gmail.com>
2025-02-20 11:38:02 -0500

    ajout de daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ function App() {
   return (
     <>
       <h1 className="text-red-400 text-2xl bg-green-400 ">Hello World</h1>
+      <p className="text-gray-600">This is a simple React app.</p>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
     </>
   );
 }
