2af2f641c99cc0fec90ab29ea4c4979c109aae38
EmnaDev <b.rached.emna@gmail.com>
2025-02-27 15:10:54 -0500

    favoritesPage

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,5 +1,29 @@
+import RecipeCard from "../components/RecipeCard";
+
 const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  const fav = true;
+  return (
+    <div className="bg-[#faf9fb] p-5 flex-1 min-h-screen">
+      <div className="max-w-screen-lg mx-auto">
+        <p className="text-3xl font-bold md:text-5xl my-4">My Favorites</p>
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
+            <img src="/404.svg" alt="404" className="h-3/4"></img>
+          </div>
+        )}
+
+        {fav && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FavoritesPage;
